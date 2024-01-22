e9f4c407660404a76cf06fc53d02609625ee23b3
muyi2905 <obaremimuyiwa@gmail.com>
2024-01-22 11:29:37 +0000

    PILLOW

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,14 @@
 # views.py
 from django.shortcuts import render
+from django.conf import settings
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+# def home(request):
+#     return render(request, 'home.html') 
 
 def home(request):
-    return render(request, 'home.html') 
+    if request.method == 'POST':
+        # Process the form data
+        image_file = request.FILES.get('image')
+        text = request.POST.get('text')
