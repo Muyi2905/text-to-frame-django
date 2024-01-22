aaaf963a8d81f5908e3e9123a0393029dc021219
muyi2905 <obaremimuyiwa@gmail.com>
2024-01-22 11:41:05 +0000

    commit

--- myapp/views.py
+++ myapp/views.py
@@ -12,3 +12,9 @@ def home(request):
         # Process the form data
         image_file = request.FILES.get('image')
         text = request.POST.get('text')
+
+        framed_image_path = os.path.join(settings.MEDIA_ROOT, 'framed_images', 'framed_image.jpg')
+        framed_image.save(framed_image_path)
+        
+        framed_image_url = os.path.join(settings.MEDIA_URL, 'framed_images', 'framed_image.jpg')
+        return render(request, 'myapp/home.html', {'framed_image_url': framed_image_url})
